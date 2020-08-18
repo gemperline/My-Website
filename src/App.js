c7bc4200cd2e4e58c2dd72cdfd3eb12e5f16378b
gemperline <39207952+gemperline@users.noreply.github.com>
2020-08-17 22:18:52 -0700

    menu anchor shadow

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ class App extends React.Component {
             <p>I'm interested in learing about new opportunities from software 
               development to web design &ndash; let's chat!
             </p>
+            <a className="email" href="mailto:adamgemperline@gmail.com">adamgemperline@gmail.com</a>
             <a href="#contact" data-toggle="modal" data-target="#message-modal"><button type="button" className="cta">Contact</button></a>
           </div>
         </div>
