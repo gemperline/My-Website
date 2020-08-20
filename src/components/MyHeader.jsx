0ce07a5a5c23d0403888dc4125129604bdce5b45
gemperline <39207952+gemperline@users.noreply.github.com>
2020-08-20 12:33:13 -0700

    ag-logo opacity

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -11,33 +11,11 @@ export function MyHeader() {
                 <div className="hero intro-content col-9">
                     <h1>Adam Gemperline</h1>
                     <h5>Software Dev | Web Design</h5>
-                    <a href="#gallery">
+                    <a href="#portfolio">
                     <button type="button" className="cta">{"See More"}</button>
                     </a>
                 </div>
             </div>
         </div>
-        // <header className="masthead text-white text-center" id="home">
-        // <div className="overlay">
-        //     <div className="container">
-        //     <div id="intro" className="container-fluid hero">
-        //         <div className="hero intro-content col-9">
-        //             <h1>Adam Gemperline</h1>
-        //             <h5>Software Dev | Web Design</h5>
-        //             <a href="#gallery">
-        //             <button type="button" className="cta">{"Coming Soon!"}</button>
-        //             </a>
-        //         </div>
-        //         </div>
-        //     </div>
-        //     <div className="row pad-top">
-        //         <div className="col-xl-9 mx-auto">
-        //         <div className="ag-logo" alt="" width="500" height="500">
-        //             <img src={AG_Logo} alt="AG"/>
-        //         </div>
-        //         </div>
-        //     </div>              
-        // </div>
-        // </header>
     );
 };
