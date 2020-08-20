99db23bc00f21405c7581f71469447161b330dd1
gemperline <39207952+gemperline@users.noreply.github.com>
2020-08-20 10:58:32 -0700

    navbar toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,31 +9,31 @@ export function CustomNavBar() {
 
     return (
         <div className="nav3">
-        <nav>
-            <div className="logo">Adam Gemperline</div>
-            <label htmlFor="btn" className="icon">
-                <MobileMenuIcon />
-            </label>
-            <input type="checkbox" id="btn"></input>
-            <ul>
-                <li className="violet"><a href="#intro">Home</a></li>
-                <li className="blue"><a href="#about">About</a></li>
-                <li className="green">
-                <label htmlFor="btn-1" className="show">Portfolio +</label>
-                <a href="#portfolio">Portfolio</a>
-                <input type="checkbox" id="btn-1"/>
-                <ul className="green-top">
-                    <li><a href="#portfolio">Gallery</a></li>
-                    <li><a href="https://github.com/gemperline" target="_blank" rel="noopener noreferrer">Adam's GitHub</a></li>
-                    <li>
-                    <label htmlFor="btn-3" className="show">Work Samples +</label>
-                    <a href="#!" id="hidden-submenu">Work Samples <CaretRight id="caret" alt=">"/></a>
-                    <input type="checkbox" id="btn-3"/>
-                    <ul>
-                        <li><a href="#!">Database Tool</a></li>
-                        <li><a href="#!">Social Web App</a></li>
-                        <li><a href="#!">Mobile</a></li>
-                    </ul>
+            <nav>
+                <div className="logo">Adam Gemperline</div>
+                <label htmlFor="btn" className="icon">
+                    <MobileMenuIcon />
+                </label>
+                <input type="checkbox" id="btn"></input>
+                <ul>
+                    <li className="violet"><a href="#intro">Home</a></li>
+                    <li className="blue"><a href="#about">About</a></li>
+                    <li className="green">
+                        <label htmlFor="btn-1" className="show">Portfolio +</label>
+                        <a href="#portfolio">Portfolio</a>
+                        <input type="checkbox" id="btn-1"/>
+                        <ul className="green-top">
+                            <li><a href="#portfolio">Gallery</a></li>
+                            <li><a href="https://github.com/gemperline" target="_blank" rel="noopener noreferrer">Adam's GitHub</a></li>
+                            <li className="">
+                            <label htmlFor="btn-3" className="show">Work Samples +</label>
+                            <a href="#!" id="hidden-submenu" className="">Work Samples <CaretRight id="caret" alt=">"/></a>
+                            <input type="checkbox" id="btn-3"/>
+                            <ul>
+                                <li><a href="#!">Database Tool</a></li>
+                                <li><a href="#!">Social Web App</a></li>
+                                <li><a href="#!">Mobile</a></li>
+                        </ul>
                     </li>
                 </ul>
                 </li>
@@ -46,9 +46,9 @@ export function CustomNavBar() {
                         <li><a href="#contact">Contact Info</a></li>
                     </ul>
                 </li>
-                <li className="orange"><a href="#!">Blog</a></li>             
-            </ul>
-        </nav>
+                    <li className="orange"><a href="#!">Blog</a></li>             
+                </ul>
+            </nav>
         </div>   
     );
 };
