d2226b41c54ad3d68e298d49ff904144267f5ddb
gemperline <39207952+gemperline@users.noreply.github.com>
2020-08-20 18:48:48 -0700

    animations, scroll speed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,15 +21,20 @@ export function About() {
         <div id="about" className="container-fluid hero white-trans">
           <h2 className="header-txt">About</h2>
           <div className="row">
-            <div className="col-md-6 left-col">
-              <h2> I'm a forward-thinking, innovative techie who likes to create technology that strengthens social interaction and promotes human development.</h2>
+            <div className="col-md-6 left-col ez-read">
+              <h3 className="my-gradient"> I'm a forward-thinking, innovative techie who likes to create technology that strengthens social interaction and promotes human development.</h3>
             </div>
-            <div className="col-md-6 right-col">
-              <h5>I was born in <a href='https://www.google.com/maps/place/Dayton,+OH/@39.8112442,-84.3422415,11z/data=!3m1!4b1!4m5!3m4!1s0x884080d5aedd1403:0xa640e392f20e4ce4!8m2!3d39.7589478!4d-84.1916069' target='_blank' rel="noopener noreferrer">Dayton, Ohio</a> and moved to the west coast in my early twenties. I have a passion for developing innovative technology that empowers people to connect with one another and improve their level of efficiency. In my free time, I enjoy camping, stargazing, attending music events, traveling the world, pressing buttons, and meeting new people.</h5>
+            <div className="col-md-6 right-col ez-read">
+              <h3 className="my-gradient">I was born in <a href='https://www.google.com/maps/place/Dayton,+OH/@39.8112442,-84.3422415,11z/data=!3m1!4b1!4m5!3m4!1s0x884080d5aedd1403:0xa640e392f20e4ce4!8m2!3d39.7589478!4d-84.1916069' target='_blank' rel="noopener noreferrer">Dayton, Ohio</a> and moved to the west coast in my early twenties. In my free time, I enjoy camping, stargazing, attending music events, traveling the world, pressing buttons, and meeting new people.</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md x-mg-100 ez-read">
+              <h3 className="my-gradient">So far, I consider myself to be a bit of a software development generalist. Most of my formal education involved applying back-end programming concepts and OOP and OOA/D techniques. However, the "Creator" in me quickly became interested in designing aesthetically-beautiful things, so I began learning front-end development. I've worked professionally with server-side and end-user langauges and tools, and I'm eager to continue tinkering my way to becoming a master.</h3>
             </div>
           </div>
             
-        <div className="row pad-top-40">
+        <div className="row pad-top-100">
             <div className="container">
                 <h3>My Toolbelt:</h3>
             </div>
